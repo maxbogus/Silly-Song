39140e37b980613c013ca0d51d841b55e980daf0
Max Boguslavskiy <max.bogus@gmail.com>
2018-01-04 05:16:58 +0300

    completed silly song application

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -9,17 +9,65 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var nameField: UITextField?
+    
+    @IBOutlet weak var lyricsView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameField?.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func reset(_ sender: Any) {
+        nameField?.text = nil
+        lyricsView.text = nil
+    }
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        guard let textField = nameField else {
+            return
+        }
+        guard let fullName: String = textField.text else {
+            return
+        }
+        lyricsView.text = lyricsForName(lyricsTemplate:bananaFanaTemplate, fullName: fullName)
+    }
+    
+    let bananaFanaTemplate = [
+        "<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+        "Banana Fana Fo F<SHORT_NAME>",
+        "Me My Mo M<SHORT_NAME>",
+        "<FULL_NAME>"].joined(separator: "\n")
+    
+    func shortNameFromName(name: String) -> String {
+        let vowels = "aeuio"
+        var lowercaseName = name.lowercased()
+        for char in name {
+            if vowels.contains(char) {
+                if let index = name.index(of: char) {
+                    lowercaseName = lowercaseName.substring(from: index)
+                    break
+                }
+            }
+        }
+        return String(lowercaseName)
+    }
+    
+    func lyricsForName(lyricsTemplate: String, fullName: String) -> String {
+        let shortName = shortNameFromName(name: fullName)
+        let lyrics = lyricsTemplate.replacingOccurrences(of: "<FULL_NAME>", with: fullName).replacingOccurrences(of: "<SHORT_NAME>", with: shortName)
+        return lyrics
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+}
